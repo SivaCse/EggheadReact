c398ec08956fedd0ae4eb93ba4605b42ac3312d2
Josh Caulfield <Coolfield@users.noreply.github.com>
2017-11-07 20:28:46 +0000

    Add prop txt

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello World</h1>
-        <p>React app with multiple nodes</p>
+        <p>{this.props.txt}</p>
         <b>bold text</b>
       </div>
     )
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App txt="this is the prop text" />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
